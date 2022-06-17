0f3293763e88cfb5d9104e2940d0c2d440ce351e
kevit <rdsankhavara2207@gmail.com>
2022-06-17 16:23:25 +0530

    C2

--- index.js
+++ index.js
@@ -36,6 +36,11 @@ app.get('/ab*cd', function (req, res) {
     res.send('Page Pattern Match');
 })
 
+app.get('/', function (req, res) {
+    console.log("Got a GET request for the homepage");
+    const userCount = 10;
+    res.send(`Hello GET. Users are: ${userCount}`);
+})
 
 var server = app.listen(8081, function () {
     var host = server.address().address
